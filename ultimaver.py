45b73cc3c7b395d0d7901ba431ae6590545801bb
cinkillo <92543114+cinkillo@users.noreply.github.com>
2024-05-13 13:26:36 +0200

    update

--- ultimaver.py
+++ ultimaver.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests
 from flask import Flask, jsonify
 from flask import request
 
@@ -6,8 +6,8 @@ lista = []
 app = Flask(__name__)
 
 wallet_owners = {
-    'CCrWZFb6shfU9bQz8pZ61VwRn9wccCViNajq67vUkaMP': 'shadow 4',
-    'G4rUK4eyvsU3ay1QafN6VZsrwmgmrBhgDR91N1YjG1eQ': 'shadow 3',
+    'CCrWZFb6shfU9bQz8pZ61VwRn9wccCViNajq67vUkaMP': 'shadow4',
+    'G4rUK4eyvsU3ay1QafN6VZsrwmgmrBhgDR91N1YjG1eQ': 'shadow3',
     '2HWT2KLLdN2wxYTqdSuko5SBzg2SEJASgV4GE2tD7TML': 'catolic',
     'DwKdQS91C9r3ue2viJsYtcFmTGDjzV91zzQqNVgDrEvb': 'unrevel',
     'B9ZyBmeXuGp2H5o6qUyYGpkVaco7s37Rd1mxA8vVeycV': 'sol7 3',
@@ -21,7 +21,7 @@ wallet_owners = {
     '62femS3raYXBmq2qQPw8SQ36FyrsMmu6GWcnUx1wcBX': 'reed-commie',
     '2mjdduwGWqVSuPhHaLruQnjeEKACDxoE7fYm5Jq1ycJL': 'feed1',
     '5J2tN3rnrh79k17qmvzQJrMPBzb6uAuG1f9gJD8qt5Tq': 'loli2',
-    'CqsdTGRW5BcuM4h6tES38c6qVctHTGNuoWDndnLZ4J5D': 'loli 3',
+    'CqsdTGRW5BcuM4h6tES38c6qVctHTGNuoWDndnLZ4J5D': 'loli3',
     '3ZKc3pVLEG4sNQBxd8Nt4Ssa8EjFNNTpioPmfJmVSGgy': 'shadow2',
     'ATTosnuUE7FHpr37uCPZVnFvfW1kRoayhtr3uy4bYJNU': 'shadow',
     'EzxYyXGBj6Z12bqBLSM3YkknZ2WmR2BVRBBe88WjBYL2': 'awp1',
@@ -41,10 +41,43 @@ wallet_owners = {
     '3MUXjjNgfUDsiEyGTKKiMMYRZhYT2coSgw8WXT6RwJft': 'dam1ah-2',
     '4Be9CvxqHW6BYiRAxW9Q3xu1ycTMWaL5z8NX4HR3ha7t': 'drawline',
     '2mC9EDWDuMn9PoXXnSmwMbHpEvowGKD9kVaBMKq2ENeQ': 'choco2',
-    'HKtzTLqANw5JjCSipquBSg5j1bUBdobAYfbBJtfvjpZW': 'shadow 6',
+    'HKtzTLqANw5JjCSipquBSg5j1bUBdobAYfbBJtfvjpZW': 'shadow6',
     '5LPVe89zDbhfpunvowczLzz8tucjQNNRSAnS8bP2uzSM': 'resolvent',
-    '6cvJDbtNTuPXoWugwjkgG4DQFWtUppcEtN3wRxYrdi8p': 'sol7-4'
+    '6cvJDbtNTuPXoWugwjkgG4DQFWtUppcEtN3wRxYrdi8p': 'sol7-4',
+    'gW2YZ1NFgmBSAMBJ2dAQK7dMKbvFE4CojyzrDKPzXYa': 'choco3',
+    '3puC5MWSCN3VerMjwiQh5E33RnTgpyVmx8w4wx46UWBY': 'choco4',
+    '5LR8fjTuwUi8qGapkdtm1wZduZsC8i8MpUH6AoZVaoou': 'choco5',
+    'GV7CfgWVCgCTfQmRfEg3r8D9BLZf3tgtxvN32hitQbXz': 'shadow7',
+    'HAmFnVktrp7VRTh3bvkfcmxkTpH48GK3iv5VLYFpGwVv': 'michimillionaire'
 }
+
+def shorten_number(num):
+    # Define suffixes for thousands, millions, and billions
+    suffixes = ['', 'k', 'M', 'B']
+
+    # Determine the appropriate suffix and divide the number
+    suffix_index = 0
+    while abs(num) >= 1000 and suffix_index < len(suffixes) - 1:
+        num /= 1000.0
+        suffix_index += 1
+
+    # Format the number with the suffix
+    formatted_num = f'{num:.1f}{suffixes[suffix_index]}'
+
+    return formatted_num
+
+def get_marketcap(CA):
+    url = f"https://api.dexscreener.com/latest/dex/tokens/{CA}"
+
+    # Make the API request
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        fdv = data['pairs'][0]['fdv']
+        return(shorten_number(fdv))
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
 
 
 @app.route('/webhook', methods=['GET', 'POST'])
@@ -57,11 +90,12 @@ def webhook():
         tokenCA = data[0]['tokenTransfers'][1]['mint']
         wallet = data[0]['feePayer']
 
+        MC = get_marketcap(tokenCA)
         wallet_owner = wallet_owners.get(wallet, 'Unknown')
 
         desc_parts = desc.split(' ')
         wallet_part = desc_parts[0]  # Assuming wallet address is the first part of the description
-        desc_parts[0] = f'({wallet_part[:4]}...{wallet_part[-4:]})'
+        desc_parts[0] = f'[({wallet_part[:4]}...{wallet_part[-4:]})](https://solscan.io/account/{wallet})'
 
         # Updated description string
         short_desc = ' '.join(desc_parts)
@@ -79,12 +113,12 @@ def webhook():
         "content": None,
         "embeds": [
             {
-            "description": f"🟢 **BUY*\n**{wallet_owner}** {short_desc}\n**CA:** `{tokenCA}`\n**Links:** [Photon](https://photon-sol.tinyastro.io/en/lp/{tokenCA}) | [Trojan](https://t.me/diomedes_trojanbot?start={tokenCA}) | [DexScreener](https://dexscreener.com/solana/{tokenCA}) | [Wallet](https://solscan.io/account/{wallet})",
-            "color": 5960611
+            "description": f"🟢 ** BUY**\n**{wallet_owner}** {short_desc}\n```{tokenCA}```\n**Market Cap: **{MC} \n**Links:** [Photon](https://photon-sol.tinyastro.io/en/lp/{tokenCA}) | [Trojan](https://t.me/diomedes_trojanbot?start={tokenCA}) | [DexScreener](https://dexscreener.com/solana/{tokenCA})",
+            "color": 5697089
             }
             ],
          "username": "Wallet Tracker",
-         "avatar_url": "https://i.pinimg.com/736x/02/ba/55/02ba55a8fae9e98b77b1c26a0737f8be.jpg",
+         "avatar_url": "https://i.imgur.com/WjWkIHW.png",
          "attachments": []}
 
         else:
@@ -93,12 +127,12 @@ def webhook():
         "content": None,
         "embeds": [
             {
-            "description": f"🔴 **SALE**\n**{wallet_owner}** {short_desc}\n**CA:** `{tokenCA}`\n**Links:** [Photon](https://photon-sol.tinyastro.io/en/lp/{tokenCA}) | [Trojan](https://t.me/diomedes_trojanbot?start={tokenCA}) | [DexScreener](https://dexscreener.com/solana/{tokenCA}) | [Wallet](https://solscan.io/account/{wallet})",
-            "color": 15948396
+            "description": f"🔴 ** SALE**\n**{wallet_owner}** {short_desc}\n```{tokenCA}```\n**Market Cap: **{MC}\n**Links:** [Photon](https://photon-sol.tinyastro.io/en/lp/{tokenCA}) | [Trojan](https://t.me/diomedes_trojanbot?start={tokenCA}) | [DexScreener](https://dexscreener.com/solana/{tokenCA})",
+            "color": 16066086
             }
             ],
          "username": "Wallet Tracker",
-         "avatar_url": "https://i.pinimg.com/736x/02/ba/55/02ba55a8fae9e98b77b1c26a0737f8be.jpg",
+         "avatar_url": "https://i.imgur.com/WjWkIHW.png",
          "attachments": []}
 
             # Enviar la solicitud POST al webhook de Discord
